b6b23484615baaf72843447d6c635f83fead5140
calebtkjordan <123994285+calebtkjordan@users.noreply.github.com>
2024-02-02 11:23:37 +0000

    fetch user data & avatar

--- assets/js/users.js
+++ assets/js/users.js
@@ -1,7 +1,5 @@
-/* global userRepository */
-
-function User(firstName, lastName, emailAddress, username, password, avatar) {
-  this.id = 1;
+function User(id, firstName, lastName, emailAddress, username, password, avatar) {
+  this.id = id;
   this.firstName = firstName;
   this.lastName = lastName;
   this.emailAddress = emailAddress;
@@ -9,3 +7,27 @@ function User(firstName, lastName, emailAddress, username, password, avatar) {
   this.password = password;
   this.avatar = avatar;
 }
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(user => {
+      var names = user.name.split(' ');
+      var firstName = names[0];
+      var lastName = names.slice(1).join(' ');
+      var usernameInLowercase = user.username.toLowerCase();
+
+      var newUser = new User(
+        user.id,
+        firstName,
+        lastName,
+        user.email,
+        usernameInLowercase,
+        'temporaryPassword',
+        'https://i.pravatar.cc/300'
+      );
+
+      userRepository.saveUser(newUser); // Use userRepository to save the user
+      console.log(newUser)
+    });
+  })
