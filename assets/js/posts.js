ab86bea6d1783730f9b4598dc8c9d994c6de7d1d
MMDPX <136532690+MMDPX@users.noreply.github.com>
2024-02-02 20:41:43 +0000

    fetching/saving placeholder posts to localstorage
    Added a function for fetching placeholder posts from jsonplaceholder and they're being saved to local storage

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -5,3 +5,24 @@ function Post(userId, content) {
   this.content = content;
   this.createdAt = dayjs().unix();
 }
+
+var JSON_PLACEHOLDER_POSTS = 'https://jsonplaceholder.typicode.com/posts?_start=0&_limit=20';
+
+
+function fetchingPlaceholderPosts() {
+  fetch(JSON_PLACEHOLDER_POSTS)
+    .then(response => {
+    if (!response.ok) {
+      throw new Error("Post cannot be fetched.");
+    }
+    return response.json();
+  })
+    .then((data) => {
+      data.forEach(post => {
+        var newPost = new Post(post.userId, post.body);
+        postRepository.createPost(newPost);
+      });
+    });
+}
+
+fetchingPlaceholderPosts();
