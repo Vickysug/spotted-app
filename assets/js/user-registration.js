b4ee887fbe290fc250b843f00c5b1aeb82612f01
JBraddockm <jbraddockm@gmail.com>
2024-02-07 03:03:24 +0000

    Fixed form validation logic.

--- assets/js/user-registration.js
+++ assets/js/user-registration.js
@@ -1,5 +1,10 @@
 /* global messageModal, serialiseFormData, userRepository, applicationContext, generatePassword  */
 
+const userRegistrationModal = $('#registration-modal');
+
+// Call-to-action button for user registration
+const registerButtonCTA = $('#register-cta');
+
 // Form element
 const userRegistrationForm = $('#registration-form');
 
@@ -10,7 +15,8 @@ const registrationDismissEl = $('#registration-dismiss');
 const registerSubmitButtonEl = $('#register-user');
 
 // Validation messages
-const passwordValidationMessageEl = $('#password-not-matched-feedback');
+const passwordValidationMessageEl = $('#password-not-valid');
+const passwordConfirmValidationMessageEl = $('#password-not-matched-feedback');
 const emailValidationMessageEl = $('#email-is-taken-feedback');
 const firstNameValidationMessageEl = $('#firstname-is-invalid-feedback');
 const lastNameValidationMessageEl = $('#lastname-is-invalid-feedback');
@@ -27,7 +33,32 @@ const confirmPasswordEl = $('#registration-confirm-password');
 // Button to generate password
 const generatePasswordButtonEl = $('#generate-password');
 
-messageModal.show();
+// messageModal.show();
+
+if (!('jwt-token' in sessionStorage)) {
+  // Displays main register button
+  registerButtonCTA.removeClass('hidden');
+}
+
+function handleResettingUserRegistrationForm() {
+  firstNameEl.attr('class', applicationContext.formInputDefaultStyle);
+  firstNameValidationMessageEl.addClass('hidden');
+
+  lastNameEl.attr('class', applicationContext.formInputDefaultStyle);
+  lastNameValidationMessageEl.addClass('hidden');
+
+  usernameEl.attr('class', applicationContext.formInputDefaultStyle);
+  usernameValidationMessageEl.addClass('hidden');
+
+  registrationEmailEl.attr('class', applicationContext.formInputDefaultStyle);
+  emailValidationMessageEl.addClass('hidden');
+
+  passwordValidationMessageEl.addClass('hidden');
+  registrationPasswordEl.attr('class', applicationContext.formInputDefaultStyle);
+
+  confirmPasswordEl.attr('class', applicationContext.formInputDefaultStyle);
+  passwordConfirmValidationMessageEl.addClass('hidden');
+}
 
 function validateRegistrationForm(userRegistrationFormDataObject) {
   // Collects errors to validate all fields at the same time
@@ -49,7 +80,7 @@ function validateRegistrationForm(userRegistrationFormDataObject) {
     firstNameValidationMessageEl.text('First name is empty');
     errors.push('First name is empty');
   } else {
-    firstNameEl.attr('class', applicationContext.formInputDefaultStyle);
+    firstNameEl.attr('class', applicationContext.formValidationSuccess);
     firstNameValidationMessageEl.addClass('hidden');
   }
 
@@ -59,7 +90,7 @@ function validateRegistrationForm(userRegistrationFormDataObject) {
     lastNameValidationMessageEl.text('Last name is empty');
     errors.push('Last name is empty');
   } else {
-    lastNameEl.attr('class', applicationContext.formInputDefaultStyle);
+    lastNameEl.attr('class', applicationContext.formValidationSuccess);
     lastNameValidationMessageEl.addClass('hidden');
   }
 
@@ -69,7 +100,7 @@ function validateRegistrationForm(userRegistrationFormDataObject) {
     usernameValidationMessageEl.text('Username is not available');
     errors.push('Username is not available');
   } else {
-    usernameEl.attr('class', applicationContext.formInputDefaultStyle);
+    usernameEl.attr('class', applicationContext.formValidationSuccess);
     usernameValidationMessageEl.addClass('hidden');
   }
 
@@ -79,7 +110,7 @@ function validateRegistrationForm(userRegistrationFormDataObject) {
     usernameValidationMessageEl.text('Username is empty');
     errors.push('Username is empty');
   } else {
-    usernameEl.attr('class', applicationContext.formInputDefaultStyle);
+    usernameEl.attr('class', applicationContext.formValidationSuccess);
     usernameValidationMessageEl.addClass('hidden');
   }
 
@@ -89,7 +120,7 @@ function validateRegistrationForm(userRegistrationFormDataObject) {
     emailValidationMessageEl.text('Email address is not available');
     errors.push('Email address is not available');
   } else {
-    registrationEmailEl.attr('class', applicationContext.formInputDefaultStyle);
+    registrationEmailEl.attr('class', applicationContext.formValidationSuccess);
     emailValidationMessageEl.addClass('hidden');
   }
 
@@ -99,27 +130,32 @@ function validateRegistrationForm(userRegistrationFormDataObject) {
     emailValidationMessageEl.text('Email address is empty');
     errors.push('Email address is empty');
   } else {
-    registrationEmailEl.attr('class', applicationContext.formInputDefaultStyle);
+    registrationEmailEl.attr('class', applicationContext.formValidationSuccess);
     emailValidationMessageEl.addClass('hidden');
   }
 
-  if (registrationPassword !== confirmPassword) {
+  if (registrationPassword.trim() === '') {
+    registrationPasswordEl.attr('class', applicationContext.formValidationError);
+    passwordValidationMessageEl.removeClass('hidden');
+    passwordValidationMessageEl.text('Choose a password');
+    passwordConfirmValidationMessageEl.removeClass('hidden');
+    passwordConfirmValidationMessageEl.text('Confirm your password');
+    errors.push('Choose a password');
+  } else {
+    registrationPasswordEl.attr('class', applicationContext.formValidationSuccess);
+    passwordValidationMessageEl.addClass('hidden');
+    passwordConfirmValidationMessageEl.addClass('hidden');
+  }
+
+  if (registrationPassword !== confirmPassword || confirmPassword.trim() === '') {
     confirmPasswordEl.attr('class', applicationContext.formValidationError);
+    registrationPasswordEl.attr('class', applicationContext.formValidationError);
     passwordValidationMessageEl.removeClass('hidden');
     passwordValidationMessageEl.text('Password does not match');
     errors.push('Password does not match');
   } else {
-    confirmPasswordEl.attr('class', applicationContext.formInputDefaultStyle);
-    passwordValidationMessageEl.addClass('hidden');
-  }
-
-  if (registrationPassword === '') {
-    registrationPasswordEl.attr('class', applicationContext.formValidationError);
-    passwordValidationMessageEl.removeClass('hidden');
-    passwordValidationMessageEl.text('Choose a password');
-    errors.push('Choose a password');
-  } else {
-    registrationPasswordEl.attr('class', applicationContext.formInputDefaultStyle);
+    registrationPasswordEl.attr('class', applicationContext.formValidationSuccess);
+    confirmPasswordEl.attr('class', applicationContext.formValidationSuccess);
     passwordValidationMessageEl.addClass('hidden');
   }
 
@@ -142,10 +178,17 @@ async function handleUserRegistration(event) {
     console.log(userRegistrationFormDataObject);
   } catch (error) {
     // Handles any errors that may occur during the signing process
-    console.error('Error occurred user registration:', error);
+    console.error('Error occurred during user registration:', error);
     $('#registration-invalid-feedback').removeClass('hidden').text('An unexpected error occurred. Please try again later.');
   }
 }
+
+function handleUserRegistrationModal() {
+  userRegistrationModal.removeClass('hidden');
+  return messageModal.show();
+}
+
+registerButtonCTA.on('click', handleUserRegistrationModal);
 
 userRegistrationForm.on('submit', registerSubmitButtonEl, handleUserRegistration);
 
@@ -156,4 +199,8 @@ generatePasswordButtonEl.on('click', (event) => {
   confirmPasswordEl.val(generatedPassword);
 });
 
-registrationDismissEl.on('click', () => messageModal.hide());
+registrationDismissEl.on('click', () => {
+  messageModal.hide();
+  handleResettingUserRegistrationForm();
+  userRegistrationModal.addClass('hidden');
+});
