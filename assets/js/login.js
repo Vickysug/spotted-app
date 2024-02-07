f20d1f70487c29f26aad98332d5e7a21cd368cab
JBraddockm <jbraddockm@gmail.com>
2024-02-07 01:28:57 +0000

    Added applicationContext to global variables

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,5 @@
 /* global securityContext, userRepository, messageModal,
-serialiseFormData, userSettingsMenuDropdown, delay  */
+serialiseFormData, userSettingsMenuDropdown, delay, applicationContext */
 
 const loginButtonCTA = $('#login-cta'); // Call-to-action button for user login
 const loginButtonEl = $('#login-user'); // Main login button
