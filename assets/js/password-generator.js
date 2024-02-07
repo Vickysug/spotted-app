a794bf5736271692b104a12be5ed760297a2e9da
JBraddockm <jbraddockm@gmail.com>
2024-02-07 00:41:00 +0000

    Refactor generatePassword function.

--- assets/js/password-generator.js
+++ assets/js/password-generator.js
@@ -1,112 +1,31 @@
-// Array of special characters to be included in password
-const specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.',
-];
-
-// Array of numeric characters to be included in password
+const specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-// Array of lowercase characters to be included in password
-const lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
-];
-
-// Array of uppercase characters to be included in password
-const upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
-];
-
-const allArrays = specialCharacters.concat(numericCharacters, lowerCasedCharacters, upperCasedCharacters);
+const allCharacterTypesArr = specialCharacters.concat(
+  numericCharacters,
+  lowerCasedCharacters,
+  upperCasedCharacters,
+);
 
 // Fisher-Yates Sorting Algorithm
 function shuffledArrays(array) {
-  for (let i = array.length - 1; i > 0; i--) {
+  for (let i = array.length - 1; i > 0; i -= 1) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
 }
 
-// Generating the password
-function getPassword() {
-  let passwordOutput = [];
-   for (let i = 0; i < 8; i++) { 
-    const randomElement = allArrays[Math.floor(Math.random() * allArrays.length)];
+// Generates password
+function generatePassword() {
+  const passwordOutput = [];
+  for (let i = 0; i <= 8; i += 1) {
+    const randomElement = allCharacterTypesArr[Math.floor(
+      Math.random() * allCharacterTypesArr.length,
+    )];
     passwordOutput.push(randomElement);
   }
   shuffledArrays(passwordOutput);
-  return console.log(passwordOutput.join(""));
+  return passwordOutput.join('');
 }
-
-getPassword();
